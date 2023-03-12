860e364167bda1a82fade58aaed926e919a22c52
pavanreddy21 <pavanreddygottimukkula@gmail.com>
2023-03-11 21:07:59 -0600

    add filer

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -5,16 +5,17 @@ import Papa from 'papaparse';
 
 const SavedPrompts = () => {
     const [prompts, setPrompts] = React.useState([]);
+    const [gitPrompts, setGitPrompts] = useState([]);
+    const [searchText, setSearchText] = useState('');
 
+    // fetch local prompts and set state
     React.useEffect(() => {
         chrome.storage.sync.get(["prompts"], (result) => {
             setPrompts(result.prompts || []);
         });
     }, []);
 
-    const [gitPrompts, setGitPropmts] = useState([]);
-
-
+    // fetch github prompts and set state
     React.useEffect(() => {
         const fetchPrompts = async () => {
             const response = await fetch(
@@ -28,31 +29,12 @@ const SavedPrompts = () => {
                 source: "github"
             }));
 
-            setGitPropmts(csvPrompts)
+            setGitPrompts(csvPrompts);
         };
         fetchPrompts();
     }, []);
 
-    React.useEffect(() => {
-        const fetchPrompts = async () => {
-            const response = await fetch(
-                "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv"
-            );
-            const csvText = await response.text();
-            const parsedCsv = Papa.parse(csvText, { header: true });
-            const csvPrompts = parsedCsv.data.map((row) => ({
-                id: row.id,
-                value: row.prompt,
-                source: "github",
-            }));
-            chrome.storage.sync.get(["prompts"], (result) => {
-                const localPrompts = result.prompts || [];
-                setPrompts([...localPrompts, ...csvPrompts]);
-            });
-        };
-        fetchPrompts();
-    }, []);
-
+    // delete prompt handler
     const handleDeletePrompt = (id) => {
         chrome.storage.sync.get(['prompts'], (result) => {
             const prompts = result.prompts || [];
@@ -63,6 +45,7 @@ const SavedPrompts = () => {
         });
     };
 
+    // copy to clipboard function
     const copyToClipboard = (text) => {
         const textField = document.createElement("textarea");
         textField.innerText = text;
@@ -72,14 +55,23 @@ const SavedPrompts = () => {
         textField.remove();
     };
 
+    // memoized combined prompts array
     const combinedPrompts = useMemo(() => {
         return [...prompts, ...gitPrompts];
     }, [prompts, gitPrompts]);
-    
+
+    // filtered prompts based on search text
+    const filteredPrompts = combinedPrompts.filter((prompt) => {
+        return prompt.value.toLowerCase().includes(searchText.toLowerCase());
+    });
 
     return (
         <>
-            {combinedPrompts.map((prompt) => {
+            <div className="p-4">
+                <label htmlFor="search" className="block mb-2 text-sm font-medium">Search Prompts</label>
+                <input type="text" id="search" className="block p-2.5 w-full text-sm border" value={searchText} onChange={(event) => setSearchText(event.target.value)} placeholder="Enter search text ... " />
+            </div>
+            {filteredPrompts.map((prompt) => {
                 return (
                     <div key={prompt.id} className="relative justify-between rounded-xl border border-gray-100 p-4 pr-8 shadow-sm mb-2">
                         <button onClick={() => copyToClipboard(prompt.value)} className="absolute top-4 left-4 h-4 w-4">
@@ -129,8 +121,8 @@ const App = () => {
             <div className="flex border-b mb-4 font-medium text-lg">
                 <button
                     className={`py-2 px-4 font-semibold ${selectedTab === 0
-                            ? "border-b-2 border-blue-500"
-                            : "border-b-2 border-transparent hover:border-blue-500"
+                        ? "border-b-2 border-blue-500"
+                        : "border-b-2 border-transparent hover:border-blue-500"
                         }`}
                     onClick={() => handleTabSelect(0)}
                 >
@@ -138,8 +130,8 @@ const App = () => {
                 </button>
                 <button
                     className={`py-2 px-4 font-semibold ${selectedTab === 1
-                            ? "border-b-2 border-blue-500"
-                            : "border-b-2 border-transparent hover:border-blue-500"
+                        ? "border-b-2 border-blue-500"
+                        : "border-b-2 border-transparent hover:border-blue-500"
                         }`}
                     onClick={() => handleTabSelect(1)}
                 >
