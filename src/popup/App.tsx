174750135ac981b17b81eb86767bde26c515ddb6
pavanreddy21 <pavanreddygottimukkula@gmail.com>
2023-03-14 12:15:36 -0500

    fix footer

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Footer from "../components/Footer";
 import Tabs from "../components/Tabs";
 import './index.css';
 
 
 const App = () => {
     return (
-        <Tabs />
+        <>
+            <Tabs />
+            <Footer />
+        </>
     );
 };
 
